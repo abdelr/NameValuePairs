2927116affdfc2264f8063eb97625fe995796907
abdelr <abdelr1982@gmail.com>
2020-04-01 22:50:19 +0200

    Adding FilterSettings entity

---
+++ NameValuePairs/NameValuePairs/Core/Entities/FilterSetting.cs
@@ -0,0 +1,27 @@
+﻿namespace NameValuePairs.Core.Entities
+{
+    /// <summary>
+    /// Filter setting Entity
+    /// </summary>
+    public class FilterSetting
+    {
+        /// <summary>
+        /// Filter on
+        /// </summary>
+        public enum ValueType
+        {
+            NAME,
+            VALUE
+        }
+
+        /// <summary>
+        /// Whether to filter on name or value
+        /// </summary>
+        public ValueType Type { get; set; }
+
+        /// <summary>
+        /// Value to match
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
