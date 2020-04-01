e18b0922bb68a44ba8a0fadbe3d5c8ea2cfca4b1
abdelr <abdelr1982@gmail.com>
2020-04-01 22:40:19 +0200

    Adding NameValuePair entity

---
+++ NameValuePairs/NameValuePairs/Core/Entities/NameValuePair.cs
@@ -0,0 +1,18 @@
+﻿namespace NameValuePairs.Core.Entities
+{
+    /// <summary>
+    /// NameValuePair Entity
+    /// </summary>
+    public class NameValuePair
+    {
+        /// <summary>
+        /// Defines the name of the pair
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Defines the value of the pair
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
