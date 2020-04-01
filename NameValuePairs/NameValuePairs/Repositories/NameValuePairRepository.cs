6d9770214fab1f45939b623d40da5cce0eb53583
abdelr <abdelr1982@gmail.com>
2020-04-01 23:28:12 +0200

    Adding implementation of NameValuePairRepository

--- NameValuePairs/NameValuePairs/Repositories/NameValuePairRepository.cs
+++ NameValuePairs/NameValuePairs/Repositories/NameValuePairRepository.cs
@@ -1,47 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using NameValuePairs.Core.Constraints;
+﻿using NameValuePairs.Core.Constraints;
 using NameValuePairs.Core.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NameValuePairs.Repositories
 {
+    /// <summary>
+    /// Repository managing the NameValuePairs
+    /// </summary>
     public class NameValuePairRepository : INameValuePairRepository
     {
-        private readonly INameValuePairConstraints nameValuePairConstraints;
+        private readonly List<NameValuePair> Elements = new List<NameValuePair>();
+        private readonly INameValuePairConstraints NameValuePairConstraints;
 
         public NameValuePairRepository(INameValuePairConstraints nameValuePairConstraints)
         {
-            
+            NameValuePairConstraints = nameValuePairConstraints;
         }
 
+        /// <summary>
+        /// Checks constraints and Adds a new NameValuePair
+        /// </summary>
         public void Add(NameValuePair pair)
         {
-            throw new NotImplementedException();
+            Elements.Add(NameValuePairConstraints.Match(pair));
         }
 
-        public IList<NameValuePair> Filter(FilterSetting setting = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IList<NameValuePair> List()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Removes a range of NameValuePairs
+        /// </summary>
         public void RemoveRange(IList<NameValuePair> range)
         {
-            throw new NotImplementedException();
+            foreach (var item in range)
+                Elements.Remove(item);
         }
 
+        /// <summary>
+        /// List all NameValuePairs
+        /// </summary>
+        public IList<NameValuePair> List()
+        {
+            //Returns a copy to avoid the caller to mess with the data
+            return Elements.ToList();
+        }
+
+        private IEnumerable<NameValuePair> PrepareFilterQuery(FilterSetting setting)
+        {
+            IEnumerable<NameValuePair> query = Elements;
+            if (setting != null)
+            {
+                if (setting.Type == FilterSetting.ValueType.NAME)
+                {
+                    query = query.Where(x => x.Name.Contains(setting.Value));
+                }
+                else
+                {
+                    query = query.Where(x => x.Value.Contains(setting.Value));
+                }
+            }
+            return query;
+        }
+
+        /// <summary>
+        /// List filtered NameValuePairs
+        /// </summary>
+        public IList<NameValuePair> Filter(FilterSetting setting)
+        {
+            return PrepareFilterQuery(setting).ToList();
+        }
+
+        /// <summary>
+        /// List filtered NameValuePairs sorted by name
+        /// </summary>
         public IList<NameValuePair> SortByName(FilterSetting setting = null)
         {
-            throw new NotImplementedException();
+            return PrepareFilterQuery(setting).OrderBy(x => x.Name).ToList();
         }
 
+        /// <summary>
+        /// List filtered NameValuePairs sorted by value
+        /// </summary>
         public IList<NameValuePair> SortByValue(FilterSetting setting = null)
         {
-            throw new NotImplementedException();
+            return PrepareFilterQuery(setting).OrderBy(x => x.Value).ToList();
         }
     }
 }
