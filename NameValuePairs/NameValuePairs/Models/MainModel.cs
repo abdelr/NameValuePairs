512e7279bd891c7daf9dbac5ca9957e2f7e8c895
abdelr <abdelr1982@gmail.com>
2020-04-01 23:49:39 +0200

    Linking the model class to the repository class

--- NameValuePairs/NameValuePairs/Models/MainModel.cs
+++ NameValuePairs/NameValuePairs/Models/MainModel.cs
@@ -1,6 +1,52 @@
-﻿namespace NameValuePairs.Models
+﻿using NameValuePairs.Core.Entities;
+using NameValuePairs.Repositories;
+using System.Collections.Generic;
+
+namespace NameValuePairs.Models
 {
-    class MainModel
+    public class MainModel
     {
+        private readonly INameValuePairRepository Repository;
+
+        private FilterSetting FilterSetting { get; set; }
+
+        public MainModel(INameValuePairRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public IList<NameValuePair> Add(NameValuePair pair)
+        {
+            Repository.Add(pair);
+            return ClearFilter();
+        }
+
+        public IList<NameValuePair> Filter(FilterSetting setting)
+        {
+            FilterSetting = setting;
+            return Repository.Filter(FilterSetting);
+        }
+
+        public IList<NameValuePair> ClearFilter()
+        {
+            FilterSetting = null;
+            return Repository.List();
+        }
+
+        public IList<NameValuePair> SortByName()
+        {
+            return Repository.SortByName(FilterSetting);
+        }
+
+        public IList<NameValuePair> SortByValue()
+        {
+            return Repository.SortByValue(FilterSetting);
+        }
+
+        public IList<NameValuePair> Delete(IList<NameValuePair> pairs)
+        {
+            Repository.RemoveRange(pairs);
+            return Repository.Filter(FilterSetting);
+        }
     }
 }
